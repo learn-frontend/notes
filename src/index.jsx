46eb5e1a9d5fa3f05c552ce796433c33f91eaab3
U.Mukhin <u.mukhin@novacom.by>
2021-04-19 15:00:41 +0300

    end lesson

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,58 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.scss';
 
+const NoteEditor = () => {
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="row">
+      <form className="editor col-4 offset-4" onSubmit={onSubmit}>
+        <textarea rows="5" placeholder="Введите текст заметки" />
+        <div className="editor-actions">
+          <ul className="editor-colors">
+            <li className="color" style={{ background: '#FEF3A1' }}></li>
+            <li className="color" style={{ background: '#B4FBA6' }}></li>
+            <li className="color" style={{ background: '#A6D2FB' }}></li>
+          </ul>
+          <button type="submit" className="btn btn-primary">Добавить</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+const NoteList = () => {
+  const [list] = useState([
+    { id: 1, color: '#FEF3A1', title: 'Заголовок', text: 'Текст заметки' },
+    { id: 2, color: '#FEF3A1', title: 'Заголовок 2', text: 'Текст заметки qwe' },
+    { id: 3, color: '#B4FBA6', title: 'Заголовок', text: 'Текст заметки qwe' },
+    { id: 4, color: '#A6D2FB', title: 'Заголовок 3', text: 'Текст заметки dfg ' },
+  ]);
+
+  return (
+    <div className="notes">
+      <div className="row g-3">
+        {list.map((note) => (
+          <div key={note.id} className="col-3">
+            <div className="notes-item" style={{ background: note.color }}>
+              <h5 className="notes-title">{note.title}</h5>
+              <div className="notes-text">{note.text}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 const App = () => {
   return (
-    <div>
-      test
+    <div className="app container">
+      <NoteEditor />
+      <NoteList />
     </div>
   );
 }
