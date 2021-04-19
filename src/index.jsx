4388d8ba5c773fcf74296d6b74598c7c1bd96277
U.Mukhin <u.mukhin@novacom.by>
2021-04-19 14:23:23 +0300

    init project

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.scss';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  }
+};
