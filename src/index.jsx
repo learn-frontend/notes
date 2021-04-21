ea46c3c9770f14a9f70c2ac17e6e08606500f146
U.Mukhin <u.mukhin@novacom.by>
2021-04-21 15:00:13 +0300

    end lesson 2

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,60 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.scss';
 
-const NoteEditor = () => {
+const NoteEditor = (props) => {
+  const COLOR_1 = '#FEF3A1';
+  const COLOR_2 = '#B4FBA6';
+  const COLOR_3 = '#A6D2FB';
+
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const [color, setColor] = useState(COLOR_1);
+  const [error, setError] = useState(null);
 
   const onSubmit = (event) => {
     event.preventDefault();
+    if (title || text) {
+      props.onAdd({
+        id: Date.now(),
+        title,
+        text,
+        color,
+      });
+      setTitle('');
+      setText('');
+      setColor(COLOR_1);
+    } else {
+      setError('Загловок или текст заметки обязателен для заполнения!');
+    }
+  }
+
+  const onChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    if (name === 'title') {
+      setTitle(value);
+    } else {
+      setText(value);
+    }
+    setError(null);
+  }
+
+  const onClickColor = (event) => {
+    const selectedColor = event.target.dataset.id;
+    setColor(selectedColor);
   }
 
   return (
     <div className="row">
       <form className="editor col-4 offset-4" onSubmit={onSubmit}>
-        <textarea rows="5" placeholder="Введите текст заметки" />
+        <input type="text" placeholder="Введите заголовок" value={title} onChange={onChange} name="title" />
+        <textarea rows="5" placeholder="Введите текст заметки" value={text} onChange={onChange} name="text" />
+        {error && <p className="editor-error">{error}</p>}
         <div className="editor-actions">
           <ul className="editor-colors">
-            <li className="color" style={{ background: '#FEF3A1' }}></li>
-            <li className="color" style={{ background: '#B4FBA6' }}></li>
-            <li className="color" style={{ background: '#A6D2FB' }}></li>
+            <li className={`color ${color === COLOR_1 ? 'selected' : ''}`} style={{ background: COLOR_1 }} data-id={COLOR_1} onClick={onClickColor}></li>
+            <li className={`color ${color === COLOR_2 ? 'selected' : ''}`} style={{ background: COLOR_2 }} data-id={COLOR_2} onClick={onClickColor}></li>
+            <li className={`color ${color === COLOR_3 ? 'selected' : ''}`} style={{ background: COLOR_3 }} data-id={COLOR_3} onClick={onClickColor}></li>
           </ul>
           <button type="submit" className="btn btn-primary">Добавить</button>
         </div>
@@ -26,35 +65,40 @@ const NoteEditor = () => {
   );
 }
 
-const NoteList = () => {
-  const [list] = useState([
-    { id: 1, color: '#FEF3A1', title: 'Заголовок', text: 'Текст заметки' },
-    { id: 2, color: '#FEF3A1', title: 'Заголовок 2', text: 'Текст заметки qwe' },
-    { id: 3, color: '#B4FBA6', title: 'Заголовок', text: 'Текст заметки qwe' },
-    { id: 4, color: '#A6D2FB', title: 'Заголовок 3', text: 'Текст заметки dfg ' },
-  ]);
-
-  return (
-    <div className="notes">
-      <div className="row g-3">
-        {list.map((note) => (
-          <div key={note.id} className="col-3">
-            <div className="notes-item" style={{ background: note.color }}>
-              <h5 className="notes-title">{note.title}</h5>
-              <div className="notes-text">{note.text}</div>
-            </div>
+const NoteList = (props) => (
+  <div className="notes">
+    <div className="row g-3">
+      {props.list.map((note) => (
+        <div key={note.id} className="col-3">
+          <div className="notes-item" style={{ background: note.color }}>
+            <h5 className="notes-title">{note.title}</h5>
+            <div className="notes-text">{note.text}</div>
+            <span className="notes-remove" onClick={props.onClick} data-id={note.id}>&times;</span>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
-  );
-}
+  </div>
+);
 
 const App = () => {
+  const [list, setList] = useState([]);
+
+  const onClick = (event) => {
+    const id = Number(event.target.dataset.id);
+    setList(list.filter((note) => note.id !== id));
+  }
+
+  const onAdd = (note) => {
+    const currentList = [...list];
+    currentList.unshift(note);
+    setList(currentList);
+  };
+
   return (
     <div className="app container">
-      <NoteEditor />
-      <NoteList />
+      <NoteEditor onAdd={onAdd} />
+      <NoteList list={list} onClick={onClick} />
     </div>
   );
 }
